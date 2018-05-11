fa305b8959add08f128a72776ab1133f04c48960
Marcus Berger <marcusvcberger@gmail.com>
2018-05-11 15:15:08 -0300

    add ultimos exer do java 2

---
+++ Java1/byteBank-Encapsulado/src/byteBank/Cliente.java
@@ -0,0 +1,37 @@
+package byteBank;
+
+
+public class Cliente {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	
+}
---
+++ Java1/byteBank-Encapsulado/src/byteBank/Conta.java
@@ -0,0 +1,25 @@
+package byteBank;
+
+class Conta{
+    private String titular;
+    private double saldo;
+
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+
+    public String getTitular(){
+        return titular;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+    
+   
+
+}
