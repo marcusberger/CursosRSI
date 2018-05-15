4d15c0346ca4f85bc5eaef4105ac07155514a9e6
Marcus Berger <marcusvcberger@gmail.com>
2018-05-15 11:37:51 -0300

    add exercicios

---
+++ Java3/byteBank-herdado-conta/src/TesteContas.java
@@ -0,0 +1,22 @@
+
+public class TesteContas {
+
+	public static void main(String[] args) {
+		
+		ContaCorrente cc = new ContaCorrente(111, 111);
+		
+		cc.deposita(100.0);
+		
+		ContaPoupanca cp = new ContaPoupanca(222, 222);
+		
+		cp.deposita(200.0);
+		
+		cc.transfere(50.0, cp);
+		
+		System.out.println("CC: " + cc.getSaldo());
+		
+		System.out.println("CP: " + cp.getSaldo());
+
+	}
+
+}
